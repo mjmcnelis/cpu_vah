2c8299d4fc232943c5fe74725e290b76db320256
Michael <Michael@Michael-McNeliss-MacBook-Pro.local>
2020-06-03 19:02:20 -0400

    Cleaning up python

--- python/generate_parameter_samples.sh
+++ python/generate_parameter_samples.sh
@@ -1,5 +1,5 @@
-rm -r random_model_parameters
-mkdir random_model_parameters
+rm -r model_parameters
+mkdir model_parameters
 python3 sample_model_parameters.py $1
 
 # $1 = number of samples you want to generate
