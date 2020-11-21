224347b9c6a4618418175a5ac70a273a5b40e1f9
Michael McNelis <mjmcnelis@gmail.com>
2020-11-20 22:36:38 -0500

    Delete generate_parameter_samples.sh

--- generate_parameter_samples.sh
+++
@@ -1,7 +0,0 @@
-cd python
-rm -r model_parameters
-mkdir model_parameters
-python3 sample_model_parameters.py $1
-
-# $1 = number of samples you want to generate
-# note: if no argument is given, default number of samples = 10
