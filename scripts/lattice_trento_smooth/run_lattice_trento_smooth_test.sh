dec34cf12e22d7e5974aceb5c3ee75002d5bd1a3
Michael <Michael@Michael-McNeliss-MacBook-Pro.local>
2020-11-23 00:07:54 -0500

    updating lattice trento smooth test

---
+++ scripts/lattice_trento_smooth/run_lattice_trento_smooth_test.sh
@@ -0,0 +1,34 @@
+echo
+echo 'Testing 3+1d nonconformal hydrodynamics for smooth TRENTo...'
+echo
+echo 'Hydro mode = ' $1
+echo
+
+cd ../..
+
+cp tables/energy_density/lattice_trento/smooth/e_block.dat tables
+
+cp tests/lattice_trento_smooth/parameters/$1/hydro.properties parameters
+cp tests/lattice_trento_smooth/parameters/$1/lattice.properties parameters
+cp tests/lattice_trento_smooth/parameters/$1/initial.properties parameters
+cp tests/lattice_trento_smooth/parameters/$1/Macros.h rhic/include
+cp tests/lattice_trento_smooth/parameters/$1/OpenMP.h rhic/include
+
+rm -r tests/lattice_trento_smooth/data/$1/adaptive
+rm -r tests/lattice_trento_smooth/data/$1/x_axis
+rm -r tests/lattice_trento_smooth/data/$1/z_axis
+
+mkdir tests/lattice_trento_smooth/data/$1/x_axis
+mkdir tests/lattice_trento_smooth/data/$1/z_axis
+
+sh clear_results.sh
+
+make clean
+make
+
+cd jobs/lattice_trento_smooth
+
+qsub -v HYDRO="$1" run_lattice_trento_smooth_test.pbs
+
+
+# $1 = hydro mode (vah, vh, vh2)
