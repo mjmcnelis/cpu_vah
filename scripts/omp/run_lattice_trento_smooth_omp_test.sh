4cec78dc9e20570d272d053d273e14e5920f42b2
Michael <Michael@Michael-McNeliss-MacBook-Pro.local>
2020-11-23 23:39:06 -0500

    Simplified omp test for user

--- scripts/omp/run_lattice_trento_smooth_omp_test.sh
+++ scripts/omp/run_lattice_trento_smooth_omp_test.sh
@@ -2,17 +2,17 @@ echo
 echo 'Testing simulation runtime for smooth 3+1d TRENTo with OpenMP acceleration...'
 echo
 echo 'Hydro mode =' $1
-echo 'Events =' $2
 echo
 
 cd ../..
 
-cp tests/lattice_trento_smooth/initial_profile/e_block.dat tables
+cp tables/energy_density/lattice_trento/smooth/e_block.dat tables
+
 cp tests/lattice_trento_smooth/parameters/$1/hydro.properties parameters
 cp tests/lattice_trento_smooth/parameters/$1/lattice.properties parameters
 cp tests/lattice_trento_smooth/parameters/$1/initial.properties parameters
 cp tests/lattice_trento_smooth/parameters/$1/Macros.h rhic/include
-cp omp/yes_omp/OpenMP.h rhic/include
+cp tests/lattice_trento_smooth/parameters/$1/OpenMP.h rhic/include
 
 rm -r output
 mkdir output
@@ -27,16 +27,13 @@ make
 
 cd jobs/omp
 
-for ((n = 1; n <= $2; n++))
-do
-    qsub run_omp_28_test.pbs
-    qsub run_omp_20_test.pbs
-    qsub run_omp_10_test.pbs
-    qsub run_omp_5_test.pbs
-    qsub run_omp_2_test.pbs
-done
+qsub -v HYDRO="$1" run_omp_28_test.pbs
+qsub -v HYDRO="$1" run_omp_20_test.pbs
+qsub -v HYDRO="$1" run_omp_10_test.pbs
+qsub -v HYDRO="$1" run_omp_5_test.pbs
+qsub -v HYDRO="$1" run_omp_2_test.pbs
+qsub -v HYDRO="$1" run_omp_1_test.pbs
 
-# $1 = sub-directory for hydro mode (vah, vh or vh2)
-# $2 = number of omp runs (e.g. 5)
 
+# $1 = hydro mode (vah, vh or vh2)
 
