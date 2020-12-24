00fa1379bdc94860bed40dc6e43b3e03956e8dee
Michael <Michael@Michael-McNeliss-MacBook-Pro.local>
2020-12-24 16:10:46 -0500

    Adding default parameters and manual

---
+++ scripts/conformal_trento/run_conformal_trento_local.sh
@@ -0,0 +1,34 @@
+echo
+echo 'Testing conformal hydrodynamics for smooth TRENTo...'
+echo
+echo 'Hydro mode = ' $1
+echo
+
+cd ../..
+
+cp tables/energy_density/conformal_trento/e_block.dat tables
+
+cp tests/conformal_trento/parameters/$1/hydro.properties parameters
+cp tests/conformal_trento/parameters/$1/lattice.properties parameters
+cp tests/conformal_trento/parameters/$1/initial.properties parameters
+cp tests/conformal_trento/parameters/$1/Macros.h rhic/include
+cp tests/conformal_trento/parameters/$1/OpenMP.h rhic/include
+
+rm -r tests/conformal_trento/data/$1/adaptive
+rm -r tests/conformal_trento/data/$1/x_axis
+rm -r tests/conformal_trento/data/$1/z_axis
+
+mkdir tests/conformal_trento/data/$1/x_axis
+mkdir tests/conformal_trento/data/$1/z_axis
+
+sh clear_results.sh
+make clean
+make
+./cpu_vah
+
+cp -r output/adaptive tests/conformal_trento/data/$1
+
+cd tests/conformal_trento/data/$1
+make
+
+# $1 = hydro mode (vah or vh)
