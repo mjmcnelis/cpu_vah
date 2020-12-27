6d4787a6e8418486e44ae7239ad82e13602dc623
Michael McNelis <mjmcnelis@gmail.com>
2020-12-27 16:48:19 -0500

    Update GhostCells.cpp

--- rhic/src/GhostCells.cpp
+++ rhic/src/GhostCells.cpp
@@ -10,9 +10,9 @@ inline int linear_column_index(int i, int j, int k, int nx, int ny)
 
 void set_ghost_cell_boundary_conditions(hydro_variables * const __restrict__ q, precision * const __restrict__ e, fluid_velocity * const __restrict__ u, int s, int sBC)
 {
-	e[s] = e[sBC];	// I don't need to set boundary conditions for the anisotropic variables
-	u[s] = u[sBC];
-	q[s] = q[sBC];	// s = ghost cell index	 |	sBC = physical cell at boundary index
+	e[s] = e[sBC];  // s   = index of ghost cell
+	u[s] = u[sBC];  // sBC = index of physical cell at boundary
+	q[s] = q[sBC];
 }
 
 
@@ -22,21 +22,20 @@ void set_ghost_cells(hydro_variables * const __restrict__ q, precision * const _
 	int ny = lattice.lattice_points_y;
 	int nz = lattice.lattice_points_eta;
 
-	// loop over the physical (y,z) lattice points:
-
+	// loop over physical (y,z) lattice points
 	#pragma omp parallel for collapse(2)
 	for(int j = 2; j < ny + 2; j++)
 	{
 		for(int k = 2; k < nz + 2; k++)
 		{
-			for(int i = 0; i <= 1; i++)				// set left ghost cells (x)
+			for(int i = 0; i <= 1; i++)             // set left ghost cells (x)
 			{
 				int s   = linear_column_index(i, j, k, nx + 4, ny + 4);
 				int sBC = linear_column_index(2, j, k, nx + 4, ny + 4);
 				set_ghost_cell_boundary_conditions(q, e, u, s, sBC);
 			}
 
-			for(int i = nx + 2; i <= nx + 3; i++)	// set right ghost cells (x)
+			for(int i = nx + 2; i <= nx + 3; i++)   // set right ghost cells (x)
 			{
 				int s   = linear_column_index(     i, j, k, nx + 4, ny + 4);
 				int sBC = linear_column_index(nx + 1, j, k, nx + 4, ny + 4);
@@ -45,21 +44,20 @@ void set_ghost_cells(hydro_variables * const __restrict__ q, precision * const _
 		}
 	}
 
-	// loop over the physical (x,z) lattice points:
-
+	// loop over physical (x,z) lattice points
 	#pragma omp parallel for collapse(2)
 	for(int i = 2; i < nx + 2; i++)
 	{
 		for(int k = 2; k < nz + 2; k++)
 		{
-			for(int j = 0; j <= 1; j++)				// set left ghost cells (y)
+			for(int j = 0; j <= 1; j++)             // set left ghost cells (y)
 			{
 				int s   = linear_column_index(i, j, k, nx + 4, ny + 4);
 				int sBC = linear_column_index(i, 2, k, nx + 4, ny + 4);
 				set_ghost_cell_boundary_conditions(q, e, u, s, sBC);
 			}
 
-			for(int j = ny + 2; j <= ny + 3; j++)	// set right ghost cells (y)
+			for(int j = ny + 2; j <= ny + 3; j++)   // set right ghost cells (y)
 			{
 				int s   = linear_column_index(i,      j, k, nx + 4, ny + 4);
 				int sBC = linear_column_index(i, ny + 1, k, nx + 4, ny + 4);
@@ -68,21 +66,20 @@ void set_ghost_cells(hydro_variables * const __restrict__ q, precision * const _
 		}
     }
     
-    // loop over the physical (x,y) lattice points:
-
+    // loop over physical (x,y) lattice points
     #pragma omp parallel for collapse(2)
     for(int i = 2; i < nx + 2; i++)
     {
 		for(int j = 2; j < ny + 2; j++)
 		{
-			for(int k = 0; k <= 1; k++)				// set left ghost cells (z)
+			for(int k = 0; k <= 1; k++)             // set left ghost cells (z)
 			{
 				int s   = linear_column_index(i, j, k, nx + 4, ny + 4);
 				int sBC = linear_column_index(i, j, 2, nx + 4, ny + 4);
 				set_ghost_cell_boundary_conditions(q, e, u, s, sBC);
 			}
 
-			for(int k = nz + 2; k <= nz + 3; k++)	// set right ghost cells (z)
+			for(int k = nz + 2; k <= nz + 3; k++)   // set right ghost cells (z)
 			{
 				int s   = linear_column_index(i, j,      k, nx + 4, ny + 4);
 				int sBC = linear_column_index(i, j, nz + 1, nx + 4, ny + 4);
