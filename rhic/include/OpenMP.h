a604f0e37f47108822b4370154fe89f714a9e62f
Michael <Michael@Michael-McNeliss-MacBook-Pro.local>
2020-12-31 18:25:32 -0500

    Fixed openmp

--- rhic/include/OpenMP.h
+++ rhic/include/OpenMP.h
@@ -2,10 +2,8 @@
 #ifndef OPENMP_H_
 #define OPENMP_H_
 
-//#define OPENMP 				// uncomment to use openmp acceleration
-
-#ifdef OPENMP
-#include <omp.h>			// need to have openmp support
+#ifdef _OPENMP
+#include <omp.h>
 #endif
 
 #endif
