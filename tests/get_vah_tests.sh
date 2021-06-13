213b2a3ddff54c249afdc5afc0806bfed467b96b
Michael <Michael@Michael-McNeliss-MacBook-Pro.local>
2021-06-13 00:14:27 -0400

    added vah test download script

---
+++ tests/get_vah_tests.sh
@@ -0,0 +1,16 @@
+
+git clone https://github.com/mjmcnelis/vah_tests.git
+
+mv vah_tests/conformal_bjorken.nb conformal_bjorken
+mv vah_tests/conformal_gubser.nb conformal_gubser
+mv vah_tests/conformal_trento.nb conformal_trento
+mv vah_tests/lattice_bjorken.nb lattice_bjorken
+mv vah_tests/lattice_trento_smooth.nb lattice_trento_smooth
+mv vah_tests/fireball.nb lattice_trento_fluctuating/fireball
+mv vah_tests/freezeout_x.nb lattice_trento_fluctuating/freezeout
+mv vah_tests/freezeout_z.nb lattice_trento_fluctuating/freezeout
+mv vah_tests/omp_test.nb omp_test
+
+rm -rf vah_tests
+
+
